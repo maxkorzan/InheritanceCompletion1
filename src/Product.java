303e359e7e5661babc05dc2fbcce4b206bd56cbb
GBTC440004UR <GBTC440004UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-04 16:04:53 -0500

    first commmit

---
+++ src/Product.java
@@ -0,0 +1,31 @@
+public class Product {
+
+    private String code;
+    private String description;
+    private double price;
+    protected static int count = 0; // a protected static variable
+
+    public Product() {
+        code = "";
+        description = "";
+        price = 0;
+    }
+
+    // Get and set accessors for the code, description, and price instance variables
+
+    @Override
+    public String toString() {
+        return "Code:                " + code + "\n" +
+                "Description:        " + description + "\n" +
+                "Price:              " + this.getFormattedPrice(price) + "\n";
+    }
+
+    private String getFormattedPrice() {
+        // Use the NumberFormat class to format the price to 2 decimal places
+    }
+
+    // Create public access for the count variable
+    public static int getCount() {
+        return count;
+    }
+}
